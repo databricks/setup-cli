937d4c74e7b85c7da94e5688fea0ad1e748bd879
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-03-28 19:33:25 +0200

    Test

---
+++ download.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if test -d .bin; then
+  echo "Directory .bin found; assuming bricks already downloaded"
+  exit 0
+fi
+
+# Find last successful deco build on main
+last_successful_run_id=$(
+  gh run list -b main -w release-snapshot --json 'databaseId,conclusion' |
+      jq 'limit(1; .[] | select(.conclusion == "success")) | .databaseId'
+)
+if [ -z "$last_successful_run_id" ]; then
+  echo "Unable to find last successful build"
+  exit 1
+fi
+
+gh run download $last_successful_run_id -n bricks_linux_snapshot -D .bin
