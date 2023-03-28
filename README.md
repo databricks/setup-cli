c3a34f26031f1b0f455066455d001d0c909c5ae2
Pieter Noordhuis <pieter.noordhuis@databricks.com>
2023-03-28 20:01:22 +0200

    Add composite action (#2)

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # setup-bricks
+
+This is a composite GitHub Action that sets up the Bricks CLI in your GitHub Actions workflow.
+
+As of March 2023, it installs the most recent snapshot build.
+
+## Usage
+
+Ensure you have a token with access to the [Bricks repository](https://github.com/databricks/bricks).
+
+In your GitHub Actions workflow, use the following step:
+
+```yml
+- uses: databricks/setup-bricks
+  with:
+    token: ${{ secrets.GH_TOKEN }}
+```
---
+++ download.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+if test -d .bin; then
+  echo "Directory .bin found; assuming bricks already downloaded"
+  exit 0
+fi
+
+# Find last successful deco build on main
+last_successful_run_id=$(
+  gh run list -b main -w release-snapshot --json 'databaseId,conclusion' |
+      jq 'limit(1; .[] | select(.conclusion == "success")) | .databaseId'
+)
+if [ -z "$last_successful_run_id" ]; then
+  echo "Unable to find last successful build"
+  exit 1
+fi
+
+# Determine artifact name with the right binaries for this runner.
+case $RUNNER_OS in
+Linux)
+    artifact="bricks_linux_snapshot"
+    ;;
+Windows)
+    artifact="bricks_windows_snapshot"
+    ;;
+macOS)
+    artifact="bricks_darwin_snapshot"
+    ;;
+esac
+
+gh run download $last_successful_run_id -n $artifact -D .bin
