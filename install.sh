eea92694fd9d09f7b2bba644e2a5320c532de90b
Andrew Nester <andrew.nester@databricks.com>
2026-04-20 09:51:18 +0200

    Release CLI v0.223.3

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Note: we cannot assume we're running bash and use the set -euo pipefail approach.
 set -e
 
-VERSION="0.223.2"
+VERSION="0.223.3"
 FILE="databricks_cli_$VERSION"
 
 # Include operating system in file name.
